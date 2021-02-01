50fb08d45aea469c5b2c30059ff3b52159b237b8
KATILYN WIGGINS <KATILYNSWIGGINS@GMAIL.COM>
2021-02-01 16:34:56 -0500

    added data.js

--- data.js
+++ data.js
@@ -1,5 +1,4 @@
-const place = {
-    [
+const saxapahaw = {
     id: '1',
     town: 'Saxapahaw',
     population: '1599',
@@ -7,121 +6,100 @@ const place = {
     region: 'SE',
     tagline: 'A quiet riverside village nestled in the country of north carolina.',
     attractions: 'value',
-    description: 'A quet riverside village nestled in the country of north carolina, Saxapahaw is the ideal location for a quiet weekend getaway.', 
-],
+    description: 'A quet riverside village nestled in the country of north carolina, Saxapahaw is the ideal location for a quiet weekend getaway.',
+}
 
 
-id: '2',
+const wilson = {
+    id: '2',
     town: 'Wilson',
-        population: '49272',
-            state: 'NC',
-                region: 'SE',
+    population: '49272',
+    state: 'NC',
+    region: 'SE',
+    tagline: 'Home of the Whirlygig',
+    attractions: 'Whirlygig Festival',
+    description: 'A small town with interesting charm, a whirly gig festival is held here every year',
+},
 
-                    tagline: Home of the Whirlygig
+const piedras = {
+    id: '3',
+    Town: 'Piedras Negras, Mexico',
+    population: '163,595',
+    state: 'Coahuila',
+    region: 'South',
+    tagline: 'The home of the inventor of Nachos',
+    attractions: 'International Day of the Nacho takes place every October 21',
+    description: 'Known affectionately as “the safe border” Piedras Negras is just across the US border from the town of Eagle Pass.'
+},
 
-attractions: Whirlygig Festival
+const fuita = {
+    id: '4',
+    Town: 'Fruita',
+    population: '13, 236',
+    State: 'colorado',
+    region: 'SW',
+    tagline: 'Quirky desert town that celebrates a headless chicken and their legacy',
+    attractions: 'Dinosaur Journey Museum, Mike the headless chicken',
+    description: 'Fruita Co is a spot thats easy to miss on the drive from SLC to Denver, but definitely worth the stop.It’s the home of Mike the headless chicken—a chicken who lived for 18 months after having most of his head chopped off and lived off a liquid diet from a water dropper.It’s a strangely sad story that the town celebrates annually in May.There’s even a statue of Mike located prominently in the town square.Fruita also has a pretty decently curated vintage store(the Vintage Common), a delicious pizza spot(Hot Tomato Pizza), an art and events space(Cavalcade) with delightfully out - of - tune outdoor pianos, and a dinosaur museum.It’s a weird and wonderful town.',
+}
 
-description: A small town with interesting charm, a whirly gig festival is held here every year. 
-    
-    ],
+const caveJunction = {
+    id: '5',
+    town: 'Cave Junction, Oregon',
+    population: '1883',
+    state: 'Oregon',
+    region: 'West',
+    tagline: 'Gateway to the Oregon Caves',
+    attractions: 'Out n About TreeHouse Hotel',
+    description: 'Cave Junction is located in the Illinois Valley, where, starting in the 1850s, the non - native economy depended on gold mining.After World War II, timber became the main source of income for residents.',
+}
 
-[
+const calipatria = {
+    id: '6',
+    Town: 'Calipatria',
+    Population: '7,395',
+    State: 'CA',
+    Region: 'West',
+    Tagline: 'One - of - a - kind monument in the California desert.',
+    Attractions: 'Salvation Mountain',
+    Description: 'Salvation Mountain is a literal man - made mountain 28 years in the making, covered in half a million gallons of latex paint.What started as a small monument made of dirt and painted cement became, over time, a sprawling adobe and hay - bale mountain complex, with peripheral structures made of telephone poles, tires, and car windows, as well as art cars and sculptures, all painted in a patchwork of stripes and color blocks of whatever paint was donated that week.',
+}
 
-    id: 3
-    
-    Town: Piedras Negras, Mexico
-    
-    population: 163, 595
-    
-    state: Coahuila
-    
-    region: South
-    
-    tagline: The home of the inventor of Nachos
-    
-    attractions: International Day of the Nacho takes place every October 21
-    
-    description: Known affectionately as “the safe border” Piedras Negras is just across
-    the US border from the town of Eagle Pass.
-    ],
+const stehekin = {
+    id: '7',
+    town: 'Stehekin',
+    population: '75',
+    state: 'WA',
+    region: 'West',
+    tagline: 'A remote mountain town accessible only by boat, plane or foot.',
+    attractions: 'Stehekin Pastry Company, North Cascade Lodge, Fly Fishing, Hiking',
+    description: 'A unique community nestled in the North Cascades on the banks of Lake Chelan, only accessible by boat, place or foot.Whether you are coming to Stehekin to hike and experience the North Cascades up close, or to just relax and escape the buzz of everyday life, come experience one of the most peaceful places on earth.',
+}
 
-    [
-        id: 4
-    
-    Town: Fruita
-    
-    population: 13, 236
-    
-    State: colorado
-    
-    region: SW
-    
-    tagline: Quirky desert town that celebrates a headless chicken and their legacy
-    
-    attractions: Dinosaur Journey Museum, Mike the headless chicken
-    
-    description: Fruita Co is a spot thats easy to miss on the drive from SLC to Denver, but definitely worth the stop.It’s the home of Mike the headless chicken—a chicken who lived for 18 months after having most of his head chopped off and lived off a liquid diet from a water dropper.It’s a strangely sad story that the town celebrates annually in May.There’s even a statue of Mike located prominently in the town square.Fruita also has a pretty decently curated vintage store(the Vintage Common), a delicious pizza spot(Hot Tomato Pizza), an art and events space(Cavalcade) with delightfully out - of - tune outdoor pianos, and a dinosaur museum.It’s a weird and wonderful town. 
-    
-    
-    ], 
+const seffner = {
+    id: '8',
+    town: 'Seffner',
+    population: '8, 362',
+    state: 'FL',
+    region: 'East',
+    tagline: 'Sculptural Americana',
+    attractions: 'Airstream Ranch',
+    description: 'An octet of classic chrome trailers speared into the ground act as a tribute to the silver bullets of the open road.',
+}
 
- ]
+const marfa = {
+    id: '9',
+    town: 'Marfa',
+    population: '1981',
+    state: 'TX',
+    tagline: '',
+    attractions: 'Mystery lights, El Cosmico, desert Prada',
+    description: 'Marfa is a texas town some 2 hours west of Austin. Known originally for it’s minimalist art scene, the town has gained traction as a tourist destination. Visitors to the town just might also catch a glimpse of the ‘mystery lights’ which are bizarre lights that dance around the night sky.',
+},
 
-id: 5
 
-town: Cave Junction, Oregon
+const places = [
+    marfa, seffner, stehekin, calipatria, caveJunction, fuita, piedras, wilson, saxapahaw
+];
 
-population: 1883
-
-state: Oregon
-
-region: West
-
-tagline: Gateway to the Oregon Caves
-
-attractions: Out n About TreeHouse Hotel
-
-description: Cave Junction is located in the Illinois Valley, where, starting in the 1850s, the non - native economy depended on gold mining.After World War II, timber became the main source of income for residents.
-    ],
-
-[
-    id:
-    Town: Calipatria,
-    Population: 7, 395
-    State: CA,
-    Region: West,
-    Tagline: One - of - a - kind monument in the California desert 
-    Attractions: Salvation Mountain,
-    Description: Salvation Mountain is a literal man - made mountain 28 years in the making, covered in half a million gallons of latex paint.What started as a small monument made of dirt and painted cement became, over time, a sprawling adobe and hay - bale mountain complex, with peripheral structures made of telephone poles, tires, and car windows, as well as art cars and sculptures, all painted in a patchwork of stripes and color blocks of whatever paint was donated that week.
-    ], 
-]
-id:
-Town: Stehekin
-Population: 75
-State: Wa
-Region: West
-Tagline: A remote mountain town accessible only by boat, plane or foot.
-    Attractions: Stehekin Pastry Company, North Cascade Lodge, Fly Fishing, Hiking
-Description: A unique community nestled in the North Cascades on the banks of Lake Chelan, only accessible by boat, place or foot.Whether you are coming to Stehekin to hike and experience the North Cascades up close, or to just relax and escape the buzz of everyday life, come experience one of the most peaceful places on earth. 
-],
-[
-    id:
-    Town: Seffner
-    Population: 8, 362
-    State: FL
-    Region: East
-    Tagline: Sculptural Americana
-    Attractions: Airstream Ranch
-    Description: An octet of classic chrome trailers speared into the ground act as a tribute to the silver bullets of the open road. 
-    
-],
-    [
-        id: '',
-        town: 'Marfa',
-        population: '1981',
-        state: 'TX',
-        tagline: '',
-        attractions: 'Mystery lights, El Cosmico, desert Prada',
-        description: 'Marfa is a texas town some 2 hours west of Austin. KNown originally for it’s minimalist art scene, the town has gained traction as a tourist destination. Visitors to the town just might also catch a glimpse of the ‘mystery lights’ which are bizarre lights that dance around the night sky.',
-    ], 
-};    
+export default places; 
