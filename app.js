08fbbd925029d53b2da097e040cc0c4d7e363a30
KATILYN WIGGINS <KATILYNSWIGGINS@GMAIL.COM>
2021-02-01 20:07:38 -0500

    added intro page and main page js functionality

--- app.js
+++ app.js
@@ -1,8 +1,26 @@
+import { getCart, setCart } from './cart/cart-utils.js';
+
 // import functions and grab DOM elements
-const submitButton = document.getElementById('submit-button');
+// const submitButton = document.getElementById('submit-button');
+const form = document.getElementById('input-form');
 // initialize state
 
 // set event listeners to update state and DOM
-submitButton.addEventListener('click', () => {
-    window.location = './main-page/';
+
+form.addEventListener('submit', (e) => {
+    e.preventDefault();
+
+    const formData = new FormData(form);
+    const user = {
+        name: formData.get('name'),
+        age: formData.get('age'),
+        email: formData.get('email')
+    };
+
+    getCart();
+
+    setCart(user);
+
+    location.href = './main-page/index.html';
+
 });
--- main-page/main.js
+++ main-page/main.js
@@ -1,6 +1,16 @@
+import { getCart } from '../cart/cart-utils.js';
 import places from '../data.js';
+const regionDiv = document.getElementById('region-div');
+
 
 const regionDisplay = document.querySelector('ul');
+
+const cartData = getCart();
+regionDiv.textContent = `Welcome ${cartData.name}, please select a region`;
+
+
+
+
 
 
 for (let place of places) {
